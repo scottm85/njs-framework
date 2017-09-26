b8a39160afa11b8545d9c5a9fcd4b9db0b2c91eb
Smills85 <wrxsti85@gmail.com>
2017-09-26 20:06:35 +0000

    added framework base

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+var gulp        = require("gulp"),
+    chown       = require('gulp-chown'),
+    stylus      = require("gulp-stylus"),
+    cleanCSS    = require("gulp-clean-css"),
+    nib         = require('nib'),
+    uglify      = require("gulp-uglify"),
+    rename      = require("gulp-rename"),
+    child       = require('child_process'),
+    fs          = require('fs'),
+    user        = 'ubuntu';
+
+
+// SERVER
+gulp.task('default', ['server', 'watch:styles', 'watch:scripts']);
+
+gulp.task('server', function(){
+    var date    = new Date(),
+        dateFmt = date.getMonth() + '-' + date.getDate() + '-' + date.getFullYear(),
+        server  = child.spawn('node', ['./bin/www']),
+        log     = fs.createWriteStream('./logs/server_' + dateFmt + '.log', {flags: 'a'});
+    server.stdout.pipe(log);
+    server.stderr.pipe(log);
+});
+
+
+// STYLESHEETS
+gulp.task('styles', function(){
+    gulp.src('./public/css/custom/*_src.styl')
+        .pipe(stylus({
+            use: nib(),
+            import: ['nib']
+        }))
+        .pipe(cleanCSS())
+        .pipe(rename(function(path){
+            path.basename = path.basename.split('_src')[0]
+        }))
+        .pipe(chown(user))
+        .pipe(gulp.dest('./public/css/custom/'));
+});
+
+gulp.task('watch:styles', function(){
+    gulp.watch('./public/css/custom/*_src.styl', ['styles']);
+});
+
+
+// JAVASCRIPTS
+gulp.task('scripts', function(){
+    gulp.src('./public/js/custom/*_src.js')
+        .pipe(uglify())
+        .pipe(rename(function(path){
+            path.basename = path.basename.split('_src')[0]
+        }))
+        .pipe(chown(user))
+        .pipe(gulp.dest('./public/js/custom/'));
+});
+
+gulp.task('watch:scripts', function(){
+    gulp.watch('./public/js/custom/*_src.js', ['scripts']);
+});
