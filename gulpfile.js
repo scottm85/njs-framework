2c441cfbdb4c9e53b11acb17b12111990dc6af5f
Smills85 <wrxsti85@gmail.com>
2017-09-28 16:12:56 +0000

    added jshint and several tasks to check various clientside and serverside scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,37 @@
-var gulp        = require("gulp"),
-    chown       = require('gulp-chown'),
-    stylus      = require("gulp-stylus"),
-    cleanCSS    = require("gulp-clean-css"),
-    nib         = require('nib'),
-    uglify      = require("gulp-uglify"),
-    rename      = require("gulp-rename"),
-    child       = require('child_process'),
-    fs          = require('fs'),
-    user        = 'ubuntu';
-
+const gulp      = require("gulp"),
+      chown     = require('gulp-chown'),
+      stylus    = require("gulp-stylus"),
+      cleanCSS  = require("gulp-clean-css"),
+      nib       = require('nib'),
+      uglify    = require("gulp-uglify"),
+      rename    = require("gulp-rename"),
+      child     = require('child_process'),
+      fs        = require('fs'),
+      jshint    = require('gulp-jshint'),
+      user      = 'ubuntu',
+      date      = new Date(),
+      dateFmt   = date.getMonth() + '-' + date.getDate() + '-' + date.getFullYear();
 
 console.log('||**********************|| DO NOT RUN AS SUDO ||**********************||');
 
-
 // INIT
-gulp.task('default', ['mongodb', 'server', 'watch:styles', 'watch:scripts']);
-
+gulp.task('default', ['mongodb', 'server', 'watch:styles', 'watch:scripts', 'client-lint', 'server-routes-lint', 'server-schemas-lint', 'server-config-lint']);
 
 // DATABASE
 gulp.task('mongodb', function(){
-    var date    = new Date(),
-        dateFmt = date.getMonth() + '-' + date.getDate() + '-' + date.getFullYear(),
-        server  = child.spawn('./mongod'),
+    let server  = child.spawn('./mongod'),
         log     = fs.createWriteStream('./logs/database_' + dateFmt + '.log', {flags: 'a'});
     server.stdout.pipe(log);
     server.stderr.pipe(log);
 });
 
-
 // SERVER
 gulp.task('server', function(){
-    var date    = new Date(),
-        dateFmt = date.getMonth() + '-' + date.getDate() + '-' + date.getFullYear(),
-        server  = child.spawn('nodemon', ['./bin/www']),
+    let server  = child.spawn('nodemon', ['./bin/www']),
         log     = fs.createWriteStream('./logs/server_' + dateFmt + '.log', {flags: 'a'});
     server.stdout.pipe(log);
     server.stderr.pipe(log);
 });
-
 
 // STYLESHEETS
 gulp.task('styles', function(){
@@ -49,7 +43,7 @@ gulp.task('styles', function(){
         .pipe(cleanCSS())
         .pipe(rename(function(path){
             console.log('CSS: ' + (path.basename + path.extname ) + ' was minified ****');
-            path.basename = path.basename.split('_src')[0]
+            path.basename = path.basename.split('_src')[0];
         }))
         .pipe(chown(user))
         .pipe(gulp.dest('./public/css/custom/'));
@@ -59,14 +53,13 @@ gulp.task('watch:styles', function(){
     gulp.watch('./public/css/custom/*_src.styl', ['styles']);
 });
 
-
 // JAVASCRIPTS
 gulp.task('scripts', function(){
     gulp.src('./public/js/custom/*_src.js')
         .pipe(uglify())
         .pipe(rename(function(path){
             console.log('Script: ' + (path.basename + path.extname ) + ' was uglified ****');
-            path.basename = path.basename.split('_src')[0]
+            path.basename = path.basename.split('_src')[0];
         }))
         .pipe(chown(user))
         .pipe(gulp.dest('./public/js/custom/'));
@@ -74,4 +67,29 @@ gulp.task('scripts', function(){
 
 gulp.task('watch:scripts', function(){
     gulp.watch('./public/js/custom/*_src.js', ['scripts']);
+});
+
+// LINTER
+gulp.task('client-lint', function() {
+  return gulp.src('./public/js/custom/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('server-routes-lint', function() {
+  return gulp.src('./routes/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('server-schemas-lint', function() {
+  return gulp.src('./schemas/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('server-config-lint', function() {
+  return gulp.src('./config/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
 });
